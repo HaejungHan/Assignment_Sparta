19ea4257f2e904cd3dcd98a6d0749fed6ed3648e
haejungHan <tinley705@gmail.com>
2024-04-29 14:38:50 +0900

    "Lv.1의 6번 문제풀이 미완료(푸는중....)"

--- src/AssignmentLv1/App.java
+++ src/AssignmentLv1/App.java
@@ -1,22 +1,23 @@
 package AssignmentLv1;
 
+import java.awt.*;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
 
-        // 5. 연산 결과 10개를 저장할 수 있는 배열을 선언 및 생성하고 연산의 결과를 저장합니다.
+        // 7. 연산결과가 10개로 고정되지 않고 무한이 저장될 수 있도록 소스 코드를 수정합니다.
         Scanner sc = new Scanner(System.in); // 입력값 받는 Scanner 생성
 
         int result = 0; // 사칙연산 결과 초기화
         boolean stop = false; // 반복문 실행 여부 변수 생성
-        int[] resultArr = new int[3]; // 결과값이 입력될 배열생성 resultArr = [2, 3, 4];
+        LinkedList<Integer> resultArr = new LinkedList<>();
         int index = 0;
 
         do { // do 구문은 무조건 실행
-                for (int i = 0; i < resultArr.length + 1; i++) {
 
                     System.out.print("첫 번째 숫자를 입력하세요: ");
                     int num1 = sc.nextInt(); // 첫 번째 숫자 입력값 생성
@@ -44,18 +45,8 @@ public class App {
                         System.out.println("잘못 입력하셨습니다. "); // 그 밖에는 해당 출력
                     }
                     System.out.println("결과: " + result); // 결과값 출력
-                    System.out.println("i는? " + i);
-                    System.out.println("index번호는? " + index);
-                    if(index >= 3) {
-                        for (int j = 0; j < resultArr.length - 1; j++) {
-                            resultArr[j] = resultArr[j+1];
-                            System.out.println("전체배열의 입력된 값은?" + resultArr[j]);
-                        }
-                    } else {
-                            resultArr[index] = result;
-                    }
+                    LinkedList.add(0, result);
 
-                    System.out.println(Arrays.toString(resultArr)); // 결과값이 잘 들어갔는지 확인
 //                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
 
                     System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
@@ -65,8 +56,7 @@ public class App {
                     } else { // 입력값이 exit라면
                         break; // do-while 반복문 종료
                     }
-                index++;
-                }
+
         } while (stop); // stop의 true/false 여부에 따라 do 구문 실행
     }
 }
