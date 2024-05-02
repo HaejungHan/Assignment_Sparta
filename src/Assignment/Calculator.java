040bdc21f3f0fe05987d1d3c9515704932f4648f
haejungHan <tinley705@gmail.com>
2024-05-02 18:40:47 +0900

    "Lv.2의 5번 문제풀이 완료"

--- src/Assignment/Calculator.java
+++ src/Assignment/Calculator.java
@@ -46,7 +46,7 @@ public class Calculator {
             result = num1 * num2; // result 값은 num1 * num2
         } else if (sign.equals("/")) { // 사칙연산 기호 입력값과 "/"와 일치하면
             if ((num2 > num1) || (num2 == 0)) { // num2(두번쨰 정수)가 num1보다 크거나 0일 경우
-                throw new ArithmeticException("나눗셈 연산에서 분모(두번째 정수)에" + num2 + "은 입력될 수 없습니다."); // Exception 처리 + 해당문구 출력
+                throw new ArithmeticException("나눗셈 연산에서 분모(두번째 정수)에 " + num2 + " 은 입력될 수 없습니다."); // Exception 처리 + 해당문구 출력
             } else { // 아니라면
                 result = num1 / num2; // result 값은 num1 / num2
             }
@@ -74,6 +74,10 @@ class App { // App클래스 시작
             String sign = sc.next(); // 사칙연산 입력값 생성 (char타입으로는 scanner 입력값 저장 불가)
             char operationChar = sign.charAt(0); // char타입으로 변환하여 operationStr의 0번째 인덱스의 값을 변수에 저장
 
+            int result = calculator.calculate(num1, num2, sign);// 입력값 calculate 메소드호출하여 실행
+            calculator.getResult().add(result); // getResult 메소드를 통해 값 저장
+//            System.out.println("결과 : " + calculator.getResult1()); // 결과값 확인
+//            System.out.println("저장된 값 : " + calculator.getResult()); // 배열을 통해 결과확인
 
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
             String removeValue = sc.next(); // remove 여부 입력값 생성
@@ -81,17 +85,14 @@ class App { // App클래스 시작
                 calculator.removeResult(0); // 인덱스번호 0 (가장 먼저 저장된 값) 삭제
             }
 
-//            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-//            String showResult = sc.next(); // inquiry 여부 입력값 생성
-//            if (showResult.equals("inquiry")) { // 입력값이 inquiry와 같다면
-//                for (Integer showAll : resultArr) { // 향상된 for반복문으로 배열의 값 전체 조회
-//                    System.out.println("저장된 결과 : " + showAll);
-//                }
-//            }
-            int result = calculator.calculate(num1, num2, sign);// 입력값 calculate 메소드호출하여 실행
-            calculator.getResult().add(result); // getResult 메소드를 통해 값 저장
-//            System.out.println("결과 : " + calculator.getResult1()); // 결과값 확인
-//            System.out.println("저장된 값 : " + calculator.getResult()); // 배열을 통해 결과확인
+            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+            String showResult = sc.next(); // inquiry 여부 입력값 생성
+            if (showResult.equals("inquiry")) { // 입력값이 inquiry와 같다면
+                for (Integer showAll : calculator.getResult()) { // 향상된 for반복문으로 배열의 값 전체 조회
+                    System.out.println("저장된 결과 : " + showAll);
+                }
+            }
+
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
         } while (!(sc.next().equals("exit"))); // exit 입력하지 않을 경우 do문으로 가서 실행
