73411041f709bf401d6a6e90b9a6f8d071f7de43
haejungHan <tinley705@gmail.com>
2024-05-02 16:36:59 +0900

    "Lv.2의 2번 문제풀이 완료"

---
+++ src/Assignment/App.java
@@ -0,0 +1,41 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator(); // Calculator 인스턴스 생성
+        Scanner sc = new Scanner(System.in); // 입력값 받는 Scanner 생성
+
+        do { // do 구문은 무조건 실행
+
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num1 = sc.nextInt(); // 첫 번째 숫자 입력값 생성
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = sc.nextInt(); // 두 번째 숫자 입력값 생성
+
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            String sign = sc.next(); // 사칙연산 입력값 생성 (char타입으로는 scanner 입력값 저장 불가)
+            char operationChar = sign.charAt(0); // char타입으로 변환하여 operationStr의 0번째 인덱스의 값을 변수에 저장
+
+
+//            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+//            String removeValue = sc.next(); // remove 여부 입력값 생성
+//            if (removeValue.equals("remove")) { //  입력값이 remove와 같다면
+//                resultArr.remove(0); // 인덱스번호 0 (가장 먼저 저장된 값) 삭제
+////                System.out.println(resultArr.toString()); // 배열 인덱스0번의 값이 제거되었는지 확인
+//            }
+//
+//            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+//            String showResult = sc.next(); // inquiry 여부 입력값 생성
+//            if (showResult.equals("inquiry")) { // 입력값이 inquiry와 같다면
+//                for (Integer showAll : resultArr) { // 향상된 for반복문으로 배열의 값 전체 조회
+//                    System.out.println("저장된 결과 : " + showAll);
+//                }
+//            }
+            calculator.calculate(num1, num2, sign); // 입력값 calculate 메소드호출하여 실행
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+        } while (!(sc.next().equals("exit"))); // exit 입력하지 않을 경우 do문으로 가서 실행
+    }
+}
--- src/Assignment/Calculator.java
+++ src/Assignment/Calculator.java
@@ -1,29 +1,19 @@
-package AssignmentLv2;
+package Assignment;
 
 import java.util.ArrayList;
 
 public class Calculator {
-
-    public static void main(String[] args) throws ArithmeticException {
-
-        ArrayList<Integer> resultArr = new ArrayList<>();  // result 값 저장할 Arraylist 생성 + 선언
-
-        Calculator cal = new Calculator(); // cal 인스턴스 생성 + 선언
-        int result = cal.calculate(3, 8, "*"); // calculate 메소드 호출 및 매개변수 대입
-
-        resultArr.add(result); // 결과값을 resultArr에 저장
-//        System.out.println(resultArr.toString()); // 배열에 값이 잘 들어갔는지 확인
-    }
-
-
+    ArrayList<Integer> resultArr = new ArrayList<>();// result 값 저장할 Arraylist 생성 + 선언
     // 속성(필드) 영역
-    int num1; // 입력값 정수 1
-    int num2; // 입력값 정수 2
-    String sign; // 사칙연산 기호
-    int result; // 사칙연산 결과
+    // 상태 데이터 영역
+    int num1 = 0; // 입력값 정수 1
+    int num2 = 0; // 입력값 정수 2
+    String sign = ""; // 사칙연산 기호
+    int result;
 
     // 생성자 영역
-    public Calculator() { // 기본생성자 생성
+    public Calculator() {
+
     }
 
     // 메소드 영역
@@ -45,6 +35,8 @@ public class Calculator {
             throw new ArithmeticException("연산기호를 잘못 입력하셨습니다."); // Exception 처리 + 해당문구 출력
         }
         System.out.println("결과: " + result);
+        resultArr.add(result); // 결과값 저장
+//        System.out.println("저장된 배열 : " + resultArr); // 배열에 저장되었는지 확인
         return result; // result 반환
     }
 
